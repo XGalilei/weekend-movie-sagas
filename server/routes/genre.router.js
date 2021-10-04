1eff755e1794258f5ca21c6ade66840292133ed9
XGalilei <ianbush1@gmail.com>
2021-10-03 19:43:18 -0500

    Added in genre router method

--- server/routes/genre.router.js
+++ server/routes/genre.router.js
@@ -4,7 +4,13 @@ const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
   // Add query to get all genres
-  res.sendStatus(500)
+  const query = 'SELECT * FROM genres ORDER BY name ASC';
+  pool.query(query).then(result => {
+    res.send(result.rows);
+  }).catch(error => {
+    console.log('ERROR: Get all genres', error);
+    res.sendStatus(500);
+  })
 });
 
 module.exports = router;
